fefde2ec1cc19fe0406a094fe76c9d7fc5cc8b2f
BuildTools <unconfigured@null.spigotmc.org>
2018-12-15 13:46:23 +0100

    Added KeepAlive to DataNode

--- src/hdfs/DataNode.java
+++ src/hdfs/DataNode.java
@@ -3,22 +3,52 @@ package hdfs;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Inet4Address;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-public class DataNode {
+class DataNode {
+
+    private class KeepAlive extends Thread{
+
+        private Socket s;
+
+        KeepAlive(Socket s) {
+            this.s = s;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ObjectOutputStream oos = new ObjectOutputStream(this.s.getOutputStream());
+                oos.writeObject(Inet4Address.getLocalHost());
+                this.s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     private File dir;
 
-	public DataNode(String dir) throws NotDirectoryException {
+	DataNode(String dir, Inet4Address nameNode, int port) throws IOException {
 	    File f = new File(dir);
+
 	    if ( !f.exists() || !f.isDirectory() ) {
 	        throw new NotDirectoryException("Le dossier n'existe pas");
         }
 	    this.dir = f;
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(new KeepAlive(new Socket(nameNode, port)), 5, 5, TimeUnit.SECONDS);
     }
 
     private String readFile(String path) throws IOException {
@@ -44,7 +74,7 @@ public class DataNode {
      * @return contents
      * @throws FileNotFoundException exception if there is not file found
      */
-	public String getChunk(String fname, int chunk) throws FileNotFoundException {
+    String getChunk(String fname, int chunk) throws FileNotFoundException {
 	    String name = this.makeName(fname, chunk);
         File[] files = this.getFiles();
 	    for (File f : files) {
@@ -64,7 +94,7 @@ public class DataNode {
      * @param chunk chunk number
      * @throws IOException exception if there is not file found or delete is impossible
      */
-	public void delChunk(String fname, int chunk) throws IOException {
+    void delChunk(String fname, int chunk) throws IOException {
         String name = this.makeName(fname, chunk);
         File[] files = this.getFiles();
         for (File f : files) {
@@ -86,7 +116,7 @@ public class DataNode {
      * @param content content of the file
      * @throws IOException exception if writing is impossible
      */
-    public void addChunk(String fname, int chunk, String content) throws IOException {
+    void addChunk(String fname, int chunk, String content) throws IOException {
         Path file = Paths.get(this.makeName(fname, chunk));
         Files.write(file, Collections.singleton(content));
     }
