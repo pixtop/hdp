280534cfc0f93f779724dbb43cfe32f5e839964e
BuildTools <unconfigured@null.spigotmc.org>
2018-12-15 19:31:07 +0100

    Added test to DataNode

---
+++ test/hdfs/DataNodeTest.java
@@ -0,0 +1,62 @@
+package hdfs;
+
+import junit.framework.TestCase;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class DataNodeTest extends TestCase {
+
+    private DataNode dataNode;
+    private String getContent, addContent;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        this.dataNode = new DataNode("./data");
+        this.getContent = new String(Files.readAllBytes(Paths.get("./data/test.0")));
+        this.addContent = new String(Files.readAllBytes(Paths.get("./data/filesample.txt")));
+        if (Files.notExists(Paths.get("./data/test.2"))) {
+            Files.createFile(Paths.get("./data/test.2"));
+        }
+    }
+
+    public void tearDown() throws Exception {
+        if (!Files.notExists(Paths.get("./data/test.1"))) {
+            Files.delete(Paths.get("./data/test.1"));
+        }
+        if (!Files.notExists(Paths.get("./data/test.2"))) {
+            Files.delete(Paths.get("./data/test.2"));
+        }
+    }
+
+    public void testGetChunk() {
+        try {
+            assertEquals(this.getContent, this.dataNode.getChunk("test", 0));
+        } catch (FileNotFoundException e) {
+            fail("Not found test.0");
+        }
+    }
+
+    public void testAddChunk() {
+        try {
+            this.dataNode.addChunk("test", 1, this.addContent);
+        } catch (IOException e) {
+            fail("Writing error");
+        }
+        assertFalse("Not found writing data", Files.notExists(Paths.get("./data/test.1")));
+    }
+
+    public void testDelChunk() {
+        try {
+            this.dataNode.delChunk("test", 2);
+        } catch (FileNotFoundException e) {
+            fail("Not found file to delete");
+        } catch (IOException e) {
+            fail("File impossible to delete");
+        }
+        assertTrue("Not found writing data", Files.notExists(Paths.get("./data/test.2")));
+    }
+}
