96e9a8c55c9b81677416cb1886777cf6bf05a9ad
Dubois Martin <mdubois5@localhost.localdomain>
2019-01-15 17:43:18 +0100

    Ajout Exception job

---
+++ src/exceptions/ErreurJobException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class ErreurJobException extends Exception {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -3467805248467775246L;
+
+	public ErreurJobException(){
+		super();
+	}
+}
